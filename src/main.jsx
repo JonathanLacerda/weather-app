141c6bd72ad450059d6bb5b23dc2d2a15e2037db
JonathanLacerda <jonathancutrimlacerda@gmail.com>
2022-04-04 15:32:18 -0300

    fix: General Adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { 
+  useEffect, 
+  useState, 
+  useRef 
+} from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { 
+GlobalStyle ,
+Wrapper,
+BackgroundAppContainer,
+WindHumContainer,
+GraphicContainer,
+MinMaxContainer,
+DiarySliderConteiner,
+Title,
+Loader
+} from './styles/styles';
+import './index.css';
+
+import HourSlider from './components/HourSlider/HourSlider';
+import DailyChart from './components/DailyChart/DailyChart';
+import MaxMin from './components/MaxMin/MaxMin';
+
+import { BsDropletHalf } from 'react-icons/bs';
+
+import {WiStrongWind} from 'react-icons/wi';
+
+import { apiWeather } from './services/apiWeather'
+import { apiGeoLocation } from './services/apiGeoLocation'
+
+
+function App() {
+const didMount = useRef(false);
+const [dataWeather, setDataWeather] = useState({});
+const [dataUser, setDataUser] = useState({});
+const load = Object.keys(dataWeather).length > 1;
+
+  useEffect(() => {
+      apiGeoLocation().then((res) => {
+          setDataUser(res);
+      });
+  },[]);
+
+  useEffect(() => {
+      if (didMount.current) {
+          apiWeather(`lat=${dataUser.latitude}&lon=${dataUser.longitude}`).then((res) => {
+              setDataWeather(res);
+          });
+      } else {
+          didMount.current = true;
+      }
+  },[dataUser]);
+
+return (
+<>
+  <GlobalStyle/>
+  <BackgroundAppContainer>
+      <Wrapper className={`container container-wind mx-auto text-center font-bold ${!load ? 'flex items-center' : ''}`}>
+          { load ? 
+              <>
+                  <h2 className='text-4xl font-thin2'>
+                      {dataUser.city} 
+                  </h2>
+                  <span className='text-6xl m-4 block text-condensed font-thin'>
+                      {Math.round(dataWeather.current.temp)}°
+                  </span>
+                  <span className='m-2 block'>
+                      Feels like {Math.round(dataWeather.current.feels_like)}°
+                  </span>
+                  <MinMaxContainer>
+                      <MaxMin data={dataWeather.daily[0]}/>
+                  </MinMaxContainer>
+                  <WindHumContainer>
+                      <div>
+                          <WiStrongWind className='inline wind-font'/> {dataWeather.current.wind_speed} km/h
+                      </div>
+                      <div>
+                          <BsDropletHalf className='inline'/> {Math.round(dataWeather.current.humidity)} %
+                      </div>
+                  </WindHumContainer>
+                  <Title>
+                      Next Hours
+                      <span></span>
+                  </Title>
+                  <DiarySliderConteiner>
+                      <HourSlider data={dataWeather.hourly}/>
+                  </DiarySliderConteiner>
+                  <Title>
+                      Next 7 Days
+                      <span></span>
+                  </Title>
+                  <GraphicContainer>
+                      <DailyChart data={dataWeather.daily}/>
+                  </GraphicContainer>
+              </>
+          : 
+              <Loader />
+          }
+      </Wrapper>
+  </BackgroundAppContainer>
+</>
 )
+}
+
+export default App
--- src/services/apiGeoLocation.js
+++ src/services/apiGeoLocation.js
@@ -1,27 +1,8 @@
 import axios from "axios";
 
 export const apiGeoLocation = async () => {
-
-
     const URL_LOCATION = `https://geolocation-db.com/jsonp/`;
   
     const { data } = await axios.get(URL_LOCATION);
     return JSON.parse(data.replace('callback(','').replace(')',''));
-
-    // axios.get('https://geolocation-db.com/jsonp/')
-    // .then(function (res) {
-    //   const geoData = JSON.parse(res.data.replace('callback(','').replace(')',''));
-
-    //   return geoData;
-    // //   setDataUser(geoData);
-    // //   setLong(geoData.longitude);
-    // //   setLat(geoData.latitude);
-    // })
-    // .catch(function (error) {
-    //   // handle error
-    //   console.log(error);
-    // })
-    // .then(function () {
-    //   // always executed
-    // });
 };
